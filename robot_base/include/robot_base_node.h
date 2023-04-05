04e790d6a9af03d2eebd7f51811520e0cb7d1f9e
PaPaPR <36095960+PaPaPR@users.noreply.github.com>
2023-04-05 09:39:47 +0800

    robot_base: 增加初始化定位 service

--- robot_base/include/robot_base_node.h
+++ robot_base/include/robot_base_node.h
@@ -1,5 +1,7 @@
 #include <geometry_msgs/Twist.h>
+#include <robot_base/UpdateMap2Odom.h>
 #include <ros/ros.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
@@ -22,19 +24,21 @@ class robot_base_node {
   std::thread serial_recv_thread_;
   std::thread static_tf_thread_;
 
-  ros::ServiceServer update_map2odom_service_;
+  ros::ServiceServer update_map2odom_srv_;
 
-  tf2_ros::StaticTransformBroadcaster map2odom_broadcaster_;
-  geometry_msgs::TransformStamped map2odom_transformStamped_;
-  geometry_msgs::TransformStamped transformStamped_;
+  tf2_ros::StaticTransformBroadcaster map2odom_br_;
+  geometry_msgs::TransformStamped map2odom_tfS_;
+  geometry_msgs::TransformStamped baselink2odom_tfS_;
   double map_init_yaw_{0.};  // 逆时针为正
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
 
   double gimbal2chassis_x_ = 0.f;
   double gimbal2chassis_y_ = 0.f;
-  tf2_ros::TransformBroadcaster gimbal_baselink_br_;
-  geometry_msgs::TransformStamped gimbal_baselink_tfs_;
+  tf2_ros::TransformBroadcaster gimbal2baselink_br_;
+  geometry_msgs::TransformStamped gimbal2baselink_tfs_;
+  tf2_ros::TransformBroadcaster chassis_baselink_br_;
+  geometry_msgs::TransformStamped chassis2baselink_tfs_;
 
  public:
   robot_base_node();
@@ -43,5 +47,7 @@ class robot_base_node {
   void INFCallBack(const INFChassisGimbalBuf &robot_inf);
   void InitMap2OdomTF();
   void UpdateMap2OdomTF();
+  bool UpdateMap2OdomTFSRV(robot_base::UpdateMap2Odom::Request &req,
+                        robot_base::UpdateMap2Odom::Response &res);
   void SendMap2OdomTF();
 };
--- robot_base/src/robot_base_node.cpp
+++ robot_base/src/robot_base_node.cpp
@@ -3,17 +3,14 @@
 robot_base_node::robot_base_node()
     : timeout_dur_(ros::Duration(1.0)), tf_listener_(tf_buffer_) {
   ros::NodeHandle nhp("~");
-  nhp.param<double>("map_init_x",
-                    map2odom_transformStamped_.transform.translation.x, 0.);
-  nhp.param<double>("map_init_y",
-                    map2odom_transformStamped_.transform.translation.y, 0.);
+  nhp.param<double>("map_init_x", map2odom_tfS_.transform.translation.x, 0.);
+  nhp.param<double>("map_init_y", map2odom_tfS_.transform.translation.y, 0.);
   nhp.param<double>("map_init_yaw", map_init_yaw_, 0.0);
   nhp.param<double>("gimbal2chassis_x", gimbal2chassis_x_, 0.0);
   nhp.param<double>("gimbal2chassis_y", gimbal2chassis_y_, 0.0);
 
-  // update_map2odom_service_ =
-  //     nh_.advertiseService<robot_base_node>("update_map2odom_service",
-  //     std::bind(&robot_base_node::UpdateMap2OdomTF,this));
+  update_map2odom_srv_ = nh_.advertiseService(
+      "update_map2odom_service", &robot_base_node::UpdateMap2OdomTFSRV, this);
   InitMap2OdomTF();
 
   std::string serial_port;
@@ -65,7 +62,7 @@ robot_base_node::robot_base_node()
   static_tf_thread_ = std::thread([this] {
     ros::Rate rate(10.0);
     while (ros::ok()) {
-      // SendMap2OdomTF();
+      SendMap2OdomTF();
       rate.sleep();
     }
   });
@@ -80,53 +77,83 @@ void robot_base_node::ChassisCallBack(
 }
 
 void robot_base_node::INFCallBack(const INFChassisGimbalBuf &robot_inf) {
-  gimbal_baselink_tfs_.header.stamp = ros::Time::now();
-  gimbal_baselink_tfs_.header.frame_id = "laser_link";
-  gimbal_baselink_tfs_.child_frame_id = "base_link";
-  gimbal_baselink_tfs_.transform.translation.x = gimbal2chassis_x_;
-  gimbal_baselink_tfs_.transform.translation.y = gimbal2chassis_y_;
+  gimbal2baselink_tfs_.header.stamp = ros::Time::now();
+  gimbal2baselink_tfs_.header.frame_id = "laser_link";
+  gimbal2baselink_tfs_.child_frame_id = "base_link";
+  gimbal2baselink_tfs_.transform.translation.x = gimbal2chassis_x_;
+  gimbal2baselink_tfs_.transform.translation.y = gimbal2chassis_y_;
   tf2::Quaternion q;
   q.setRPY(0, 0,
            robot_inf.gimbal_chassis_yaw_dif / 180.0 * 3.14159265358979323846);
   q.normalize();
-  gimbal_baselink_tfs_.transform.rotation.x = q.x();
-  gimbal_baselink_tfs_.transform.rotation.y = q.y();
-  gimbal_baselink_tfs_.transform.rotation.z = q.z();
-  gimbal_baselink_tfs_.transform.rotation.w = q.w();
-  gimbal_baselink_br_.sendTransform(gimbal_baselink_tfs_);
+  gimbal2baselink_tfs_.transform.rotation.x = q.x();
+  gimbal2baselink_tfs_.transform.rotation.y = q.y();
+  gimbal2baselink_tfs_.transform.rotation.z = q.z();
+  gimbal2baselink_tfs_.transform.rotation.w = q.w();
+  gimbal2baselink_br_.sendTransform(gimbal2baselink_tfs_);
+
+  chassis2baselink_tfs_.header.stamp = ros::Time::now();
+  chassis2baselink_tfs_.header.frame_id = "base_link";
+  chassis2baselink_tfs_.child_frame_id = "chassis_link";
+  chassis2baselink_tfs_.transform.translation.x = 0.;
+  chassis2baselink_tfs_.transform.translation.y = 0.;
+  tf2::Quaternion chassis_tfs_q;
+  // chassis_tfs_q.setRPY(0, 0, robot_inf.);
 }
 
 void robot_base_node::InitMap2OdomTF() {
-  map2odom_transformStamped_.header.frame_id = "map2";
-  map2odom_transformStamped_.child_frame_id = "base_link";
+  map2odom_tfS_.header.frame_id = "map";
+  map2odom_tfS_.child_frame_id = "odom";
+  map2odom_tfS_.transform.translation.x = 0.;
+  map2odom_tfS_.transform.translation.y = 0.;
+  map2odom_tfS_.transform.translation.z = 0.;
+  map2odom_tfS_.transform.rotation.x = 0.;
+  map2odom_tfS_.transform.rotation.y = 0.;
+  map2odom_tfS_.transform.rotation.z = 0.;
+  map2odom_tfS_.transform.rotation.w = 0.;
 }
 
 void robot_base_node::UpdateMap2OdomTF() {
-  map2odom_transformStamped_.transform.translation =
-      transformStamped_.transform.translation;
-  static tf2::Quaternion q_rot, q_orig, q_new;
-  q_new.setRPY(0, 0, -map_init_yaw_);
-  // tf2::convert(transformStamped_.transform.rotation, q_orig);
-  // q_new = q_rot*q_orig;
+  map2odom_tfS_.transform.translation.x =
+      baselink2odom_tfS_.transform.translation.x;
+  map2odom_tfS_.transform.translation.y =
+      baselink2odom_tfS_.transform.translation.y;
+
+  tf2::Quaternion q_orig(baselink2odom_tfS_.transform.rotation.x,
+                         baselink2odom_tfS_.transform.rotation.y,
+                         baselink2odom_tfS_.transform.rotation.z,
+                         baselink2odom_tfS_.transform.rotation.w);
+  tf2::Matrix3x3 m_new(q_orig);
+  double roll, pitch, yaw;
+  m_new.getRPY(roll, pitch, yaw);
+
+  tf2::Quaternion q_new;
+  q_new.setRPY(0, 0, -map_init_yaw_ + yaw);
   q_new.normalize();
 
-  map2odom_transformStamped_.transform.rotation.x = q_new.x();
-  map2odom_transformStamped_.transform.rotation.y = q_new.y();
-  map2odom_transformStamped_.transform.rotation.z = q_new.z();
-  map2odom_transformStamped_.transform.rotation.w = q_new.w();
+  map2odom_tfS_.transform.rotation.x = q_new.x();
+  map2odom_tfS_.transform.rotation.y = q_new.y();
+  map2odom_tfS_.transform.rotation.z = q_new.z();
+  map2odom_tfS_.transform.rotation.w = q_new.w();
+}
+bool robot_base_node::UpdateMap2OdomTFSRV(
+    robot_base::UpdateMap2Odom::Request &req,
+    robot_base::UpdateMap2Odom::Response &res) {
+  UpdateMap2OdomTF();
+  return true;
 }
 
 void robot_base_node::SendMap2OdomTF() {
   try {
-    transformStamped_ =
+    baselink2odom_tfS_ =
         tf_buffer_.lookupTransform("base_link", "odom", ros::Time(0));
     UpdateMap2OdomTF();
   } catch (tf2::TransformException &ex) {
-    ROS_WARN("%s", ex.what());
+    // ROS_WARN("%s", ex.what());
   }
 
-  map2odom_transformStamped_.header.stamp = ros::Time::now();
-  map2odom_broadcaster_.sendTransform(map2odom_transformStamped_);
+  map2odom_tfS_.header.stamp = ros::Time::now();
+  map2odom_br_.sendTransform(map2odom_tfS_);
 }
 
 int main(int argc, char **argv) {
